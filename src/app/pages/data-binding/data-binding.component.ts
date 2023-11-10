949fb2aaeaee163bb4b53d3d4ca32cb83ae6c553
Vitor Azevedo <vitor.azevedo@icloud.com>
2023-11-11 00:01:19 +0100

    removend unnecessary constans and code preview

--- src/app/pages/data-binding/data-binding.component.ts
+++ src/app/pages/data-binding/data-binding.component.ts
@@ -1,29 +1,13 @@
-import { CodePreviewComponent } from 'src/app/components/code-preview/code-preview.component';
-
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { HTML, TYPESCRIPT } from './data-binding.contants';
-
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [CommonModule, RouterLink, CodePreviewComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.scss'
 })
 export class DataBindingComponent {
-  data = {
-    preview: {
-      html: {
-        language: 'html',
-        code: HTML
-      },
-      typescript: {
-        language: 'typescript',
-        code: TYPESCRIPT
-      }
-    }
-  };
 }
